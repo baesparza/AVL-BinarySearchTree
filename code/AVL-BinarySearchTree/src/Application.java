be05e74cecd8d6d12a7cf0b60dabdf9626b3dbc0
baesparza <bruno.be81@gmail.com>
2018-01-16 18:14:04 -0500

    feat(TreeNode): add struct for nodes in bst

--- code/AVL-BinarySearchTree/src/Application.java
+++ code/AVL-BinarySearchTree/src/Application.java
@@ -1,14 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
- * @author Usuario
+ * @author baesparza
  */
-public class Application {
+
+class TreeNode {
+    public TreeNode left, right;
+    public int value;
+    public TreeNode(int value) {
+        this.value = value;
+        this.left = this.right = null;
+    }
+}
+
+class Application {
 
     /**
      * @param args the command line arguments
