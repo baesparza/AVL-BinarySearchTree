bbfc5d83f1fe105519ca87055432fe498c0fd261
baesparza <bruno.be81@gmail.com>
2018-01-16 18:40:21 -0500

    feat(bst): add removeNode from tree

--- code/AVL-BinarySearchTree/src/Application.java
+++ code/AVL-BinarySearchTree/src/Application.java
@@ -21,25 +21,70 @@ class BinarySearchTree {
         this.printInOrder(tree.right);
     } 
     
-    public TreeNode addNode(TreeNode tree, int value) {
+    public TreeNode addNode(TreeNode tree, int num) {
         // check if we are in a tree
         if (tree == null) {
-            return new TreeNode(value);
+            return new TreeNode(num);
         }
         
-        /*add node left or right branch*/
+        /*add node to left or right branch*/
         // insert in left branch
-        if (value < tree.value) {
-            tree.left = this.addNode(tree.left, value);
+        if (num < tree.value) {
+            tree.left = this.addNode(tree.left, num);
             return tree;
         }
         // insert in right branch
-        if (value > tree.value) {
-            tree.right = this.addNode(tree.right, value);
+        if (num > tree.value) {
+            tree.right = this.addNode(tree.right, num);
             return tree;
         }
         
         /*node already in list*/
+        return tree;
+    }
+    
+    public TreeNode removeNode(TreeNode tree, int num) {
+        if (tree == null) return null;
+        
+        /*remove node from left or right branch*/
+        // go into left branch
+        if (num < tree.value) {
+            tree.left = this.removeNode(tree.left, num);
+            return tree;
+        }
+        // go into right branch
+        if (num > tree.value) {
+            tree.right = this.removeNode(tree.right, num);
+            return tree;
+        }
+        
+        /*remove this node*/
+        if (tree.value == num) {
+            /*check if we are in a leaf*/
+            if (tree.right == null && tree.left == null) // unlink this node
+                return null;
+            /*Search node to replace this*/
+            TreeNode current = null; // iterator to search replacement
+            if (tree.left != null) {
+                // search max value in left branch
+                current = tree.left;
+                while (current.right != null) // stop before null node
+                    current = current.right;
+                /*change values*/
+                tree.value = current.value;
+                tree.left = this.removeNode(tree.left, tree.value);
+            } else {
+                // search min value in right branch
+                current = tree.right;
+                while (current.left != null) // stop before null node
+                    current = current.left;
+                /*change values*/
+                tree.value = current.value;
+                tree.right = this.removeNode(tree.right, tree.value);
+            }
+        }
+        
+        /*Value never existed in tree*/
         return tree;
     }
 }
@@ -58,6 +103,13 @@ class Application {
         root = bst.addNode(root, 2);
         root = bst.addNode(root, 5);
         
+        System.out.println("Tree:");
+        bst.printInOrder(root);
+        
+        root = bst.removeNode(root, 5);
+        root = bst.removeNode(root, 3);
+        
+        System.out.println("Tree:");
         bst.printInOrder(root);
     }
     
