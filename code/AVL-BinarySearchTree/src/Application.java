5b6f1b4c2796b8c3779d9150cabdbd72621db8fd
baesparza <bruno.be81@gmail.com>
2018-01-16 19:21:44 -0500

    feat(AVL): initialized class with basic methods

--- code/AVL-BinarySearchTree/src/Application.java
+++ code/AVL-BinarySearchTree/src/Application.java
@@ -117,11 +117,43 @@ class BinarySearchTree {
     }
 }
 
+/**
+ * Methods for AVL-BST
+ */
+class AVL extends BinarySearchTree{
+    /**
+     * Add node to tree
+     * 
+     * @param tree were new node will be added
+     * @param num data that will be added to the tree
+     * @return tree with new node
+     */
+    @Override
+    public TreeNode addNode(TreeNode tree, int value) {
+        // add node as normal to tree
+        tree = super.addNode(tree, value);
+        return tree;
+    }
+    
+    /**
+     * Remove node from tree
+     * 
+     * @param tree were node might be to be removed
+     * @param num data needed to be removed
+     * @return tree were data was removed
+     */
+    @Override
+    public TreeNode removeNode(TreeNode tree, int value) {
+        tree = super.removeNode(tree, value);
+        return tree;
+    }
+}
+
 class Application {
     
     public static void main(String[] args) {
         // methods to work with
-        BinarySearchTree bst = new BinarySearchTree();
+        AVL bst = new AVL();
         // root of the new tree
         TreeNode root = null;
         
