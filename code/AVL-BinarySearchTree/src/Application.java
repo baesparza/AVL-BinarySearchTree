69dd8c84390426ec343513744962080f25edb050
baesparza <bruno.be81@gmail.com>
2018-01-16 19:10:02 -0500

    docs(*): add more comments

--- code/AVL-BinarySearchTree/src/Application.java
+++ code/AVL-BinarySearchTree/src/Application.java
@@ -4,23 +4,44 @@
  * @author baesparza
  */
 
+/**
+ * Struct node used for Tree
+ */
 class TreeNode {
     public TreeNode left, right;
     public int value;
+    /**
+     * Constructor initialize data and pointer to other nodes
+     * @param value data to be stored in this node
+     */
     public TreeNode(int value) {
         this.value = value;
         this.left = this.right = null;
     }
 }
 
+/**
+ * Methods to work with BST
+ */
 class BinarySearchTree {
+    /**
+     * Traverse tree in order
+     * @param tree tree to be printed
+     */
     public void printInOrder(TreeNode tree) {
+        // check if we are in a node
         if (tree == null) return;
         this.printInOrder(tree.left);
         System.out.print(tree.value + " ");
         this.printInOrder(tree.right);
     } 
     
+    /**
+     * Add new node to binary search tree
+     * @param tree were new node will be added
+     * @param num data that will be added to the tree
+     * @return tree with new node
+     */
     public TreeNode addNode(TreeNode tree, int num) {
         // check if we are in a tree
         if (tree == null) {
@@ -43,7 +64,14 @@ class BinarySearchTree {
         return tree;
     }
     
+    /**
+     * Remove node from binary search tree
+     * @param tree were node might be to be removed
+     * @param num data needed to be removed
+     * @return tree were data was removed
+     */
     public TreeNode removeNode(TreeNode tree, int num) {
+        // check if we are in a node
         if (tree == null) return null;
         
         /*remove node from left or right branch*/
@@ -90,26 +118,36 @@ class BinarySearchTree {
 }
 
 class Application {
-
+    
     public static void main(String[] args) {
-        
+        // methods to work with
         BinarySearchTree bst = new BinarySearchTree();
-        
+        // root of the new tree
         TreeNode root = null;
         
-        root = bst.addNode(root, 3);
+        /*add this nodes*/
+        root = bst.addNode(root, 7);
+        root = bst.addNode(root, 2);
+        root = bst.addNode(root, 7);
+        root = bst.addNode(root, 8);
         root = bst.addNode(root, 1);
         root = bst.addNode(root, 4);
-        root = bst.addNode(root, 2);
         root = bst.addNode(root, 5);
+        root = bst.addNode(root, 3);
+        root = bst.addNode(root, 2);
+        root = bst.addNode(root, 10);
+        root = bst.addNode(root, 9);
         
+        // print tree
         System.out.println("Tree:");
         bst.printInOrder(root);
         
+        // remove this nodes
+        root = bst.removeNode(root, 10);
         root = bst.removeNode(root, 5);
-        root = bst.removeNode(root, 3);
         
-        System.out.println("Tree:");
+        // print tree after nodes were removed
+        System.out.println("\nTree:");
         bst.printInOrder(root);
     }
     
