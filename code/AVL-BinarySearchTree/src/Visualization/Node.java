0de8de1f77a30587fba60d16563ea8fa7ab339d5
baesparza <bruno.be81@gmail.com>
2018-01-21 19:05:21 -0500

    feat(Node): add data fot plotting nodes

--- code/AVL-BinarySearchTree/src/Visualization/Node.java
+++ code/AVL-BinarySearchTree/src/Visualization/Node.java
@@ -1,5 +1,7 @@
 package Visualization;
 
+import Tree.TreeNode;
+
 /**
  *
  * @author baesparza
@@ -8,23 +10,28 @@ package Visualization;
 /**
  * Struct node used for plotting a Tree
  */
-public class TreePoint {
-    public int data;
+public class Node {
+    public TreeNode node;
     public boolean valid;
+    public int x, y, totalSize;
+    
+    
     /**
      * COnstructor when point exists
      * @param data 
      */
-    public TreePoint(int data) {
-        this.data = data;
+    public Node(TreeNode data, int totalSize ) {
+        this.node = data;
         this.valid = true;
+        this.totalSize = totalSize;
     }
     /**
      * COnstructor when point doesn't exists
      * @param valid 
      */
-    public TreePoint(boolean valid) {
+    public Node(boolean valid) {
         this.valid = valid;
+        this.node = null;
     }
 }
 
